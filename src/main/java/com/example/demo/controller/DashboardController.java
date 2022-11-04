12dfb767effc9834fb9f9e5eee2d63e3e02ebcfb
nguyenthanhloc2609 <nguyenthanhloc2609@gmail.com>
2022-11-04 17:33:40 +0700

    add metronic theme

--- src/main/java/com/example/demo/controller/DashboardController.java
+++ src/main/java/com/example/demo/controller/DashboardController.java
@@ -1,7 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class DashboardController {
+
+    @RequestMapping(value = "/clinic", method = RequestMethod.GET)
+    public String home(Model model) {
+        return "components/calendar/basic";
+    }
 }
