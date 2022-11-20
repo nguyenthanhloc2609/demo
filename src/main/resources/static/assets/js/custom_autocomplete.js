d36e499b8c890c58eb112537f5ce803c5ac35fce
locnt <nguyenthanhloc2609@gmail.com>
2022-11-20 16:16:19 +0700

    fill payment when select customer

--- src/main/resources/static/assets/js/custom_autocomplete.js
+++ src/main/resources/static/assets/js/custom_autocomplete.js
@@ -1,5 +1,5 @@
 // Class definition
-var KTTypeahead = function() {
+var KTTypeahead = function () {
 
     // var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
     //     'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
@@ -13,16 +13,16 @@ var KTTypeahead = function() {
     // ];
     // var states = [{id: "123", name: "locnt", billing: "1/2", note: ""}];
 
-    var demo2 = function() {
+    var demo2 = function () {
         // constructs the suggestion engine
         var bloodhound = new Bloodhound({
-            datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.name)},
+            datumTokenizer: function (d) { return Bloodhound.tokenizers.whitespace(d.name) },
             queryTokenizer: Bloodhound.tokenizers.whitespace,
             // `states` is an array of state names defined in "The Basics"
             // local: states
             remote: {
                 url: "customers/name?name=%QUERY",
-                filter: function(data) {
+                filter: function (data) {
                     // console.log(data)
                     return data;
                 },
@@ -31,19 +31,38 @@ var KTTypeahead = function() {
         });
 
         $('#cusName').typeahead({
-                hint: true,
-                highlight: true,
-                minLength: 3
-            },
+            hint: true,
+            highlight: true,
+            minLength: 3
+        },
             {
                 // name: 'name',
                 displayKey: 'name',
                 source: bloodhound
             });
 
-        $('#cusName').bind('typeahead:select', function(e, datum) {
+        $('#cusName').bind('typeahead:select', function (e, datum) {
             $('#cusName').typeahead('val', datum.name);
-            $('#pay').val(datum.billing);
+            var bill = datum.billing;
+            var num1, num2;
+            if (bill.includes('N')) {
+                num2 = Number(bill.split('N')[1]);
+                $('#pay').val('N' + (num2 + 1));
+            } else if (bill.includes('/')) {
+                var arr = bill.split('/');
+                num1 = Number(arr[0]);
+                num2 = Number(arr[1]);
+                if (num1 === num2) {
+                    num1++;
+                    num2++;
+                } else {
+                    num1++;
+                }
+                $('#pay').val(num1 + "/" + num2);
+            } else {
+                $('#pay').val(datum.billing);
+            }
+
             $('#note1').val(datum.note);
             $('#diagnostic').val(datum.diag);
 
@@ -53,12 +72,12 @@ var KTTypeahead = function() {
 
     return {
         // public functions
-        init: function() {
+        init: function () {
             demo2();
         }
     };
 }();
 
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
     KTTypeahead.init();
 });
