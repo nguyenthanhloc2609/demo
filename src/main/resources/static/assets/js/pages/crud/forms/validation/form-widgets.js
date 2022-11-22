0f010b760b09e5af25569a0ca50606beb5b9cbd7
nguyenthanhloc2609 <nguyenthanhloc2609@gmail.com>
2022-11-22 17:30:06 +0700

    fix bug responsive

--- src/main/resources/static/assets/js/pages/crud/forms/validation/form-widgets.js
+++ src/main/resources/static/assets/js/pages/crud/forms/validation/form-widgets.js
@@ -4,14 +4,22 @@ var KTFormWidgets = function () {
     // Private functions
     var validator;
 
-    var initWidgets = function() {
+    var initWidgets = function () {
         // datepicker
         $('#kt_datepicker').datepicker({
             todayHighlight: true,
+            autoclose: true,
+            format: 'dd/mm/yyyy',
             templates: {
                 leftArrow: '<i class="la la-angle-left"></i>',
                 rightArrow: '<i class="la la-angle-right"></i>'
             }
+        });
+
+        $('#kt_datepicker').change(function (event) {
+            // console.log(event.target.value)
+            window.location = '/tran-a-day'
+            window.localStorage.setItem("date", event.target.value);
         });
 
         // datetimepicker
@@ -22,7 +30,7 @@ var KTFormWidgets = function () {
             format: 'yyyy.mm.dd hh:ii'
         });
 
-        $('#kt_datetimepicker').change(function() {
+        $('#kt_datetimepicker').change(function () {
             validator.element($(this));
         });
 
@@ -38,22 +46,22 @@ var KTFormWidgets = function () {
             buttonClasses: ' btn',
             applyClass: 'btn-primary',
             cancelClass: 'btn-secondary'
-        }, function(start, end, label) {
+        }, function (start, end, label) {
             var input = $('#kt_daterangepicker').find('.form-control');
-            
-            input.val( start.format('YYYY/MM/DD') + ' / ' + end.format('YYYY/MM/DD'));
+
+            input.val(start.format('YYYY/MM/DD') + ' / ' + end.format('YYYY/MM/DD'));
             validator.element(input); // validate element
         });
 
         // bootstrap switch
         $('[data-switch=true]').bootstrapSwitch();
-        $('[data-switch=true]').on('switchChange.bootstrapSwitch', function() {
+        $('[data-switch=true]').on('switchChange.bootstrapSwitch', function () {
             validator.element($(this)); // validate element
         });
 
         // bootstrap select
         $('#kt_bootstrap_select').selectpicker();
-        $('#kt_bootstrap_select').on('changed.bs.select', function() {
+        $('#kt_bootstrap_select').on('changed.bs.select', function () {
             validator.element($(this)); // validate element
         });
 
@@ -61,7 +69,7 @@ var KTFormWidgets = function () {
         $('#kt_select2').select2({
             placeholder: "Select a state",
         });
-        $('#kt_select2').on('select2:change', function(){
+        $('#kt_select2').on('select2:change', function () {
             validator.element($(this)); // validate element
         });
 
@@ -76,18 +84,18 @@ var KTFormWidgets = function () {
             name: 'countries',
             source: countries
         });
-        $('#kt_typeahead').bind('typeahead:select', function(ev, suggestion) {
+        $('#kt_typeahead').bind('typeahead:select', function (ev, suggestion) {
             validator.element($('#kt_typeahead')); // validate element
         });
     }
-    
+
     var initValidation = function () {
-        validator = $( "#kt_form_1" ).validate({
+        validator = $("#kt_form_1").validate({
             // define validation rules
             rules: {
                 date: {
                     required: true,
-                    date: true 
+                    date: true
                 },
                 daterange: {
                     required: true
@@ -119,9 +127,9 @@ var KTFormWidgets = function () {
                     required: true
                 }
             },
-            
+
             //display error alert on form submit  
-            invalidHandler: function(event, validator) {             
+            invalidHandler: function (event, validator) {
                 var alert = $('#kt_form_1_msg');
                 alert.removeClass('kt--hide').show();
                 KTUtil.scrollTo('kt_form_1_msg', -200);
@@ -130,18 +138,18 @@ var KTFormWidgets = function () {
             submitHandler: function (form) {
                 //form[0].submit(); // submit the form
             }
-        });       
+        });
     }
 
     return {
         // public functions
-        init: function() {
-            initWidgets(); 
+        init: function () {
+            initWidgets();
             initValidation();
         }
     };
 }();
 
-jQuery(document).ready(function() {    
+jQuery(document).ready(function () {
     KTFormWidgets.init();
 });
