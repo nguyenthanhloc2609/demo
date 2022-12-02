4e772d405ae9a4844a2495151e10b11ae0ce8ec9
nguyenthanhloc2609 <nguyenthanhloc2609@gmail.com>
2022-12-02 17:11:42 +0700

    redirect to today

--- src/main/resources/static/assets/js/pages/components/calendar/basic.js
+++ src/main/resources/static/assets/js/pages/components/calendar/basic.js
@@ -109,9 +109,9 @@ function formatDate(date) {
 
     return [day, month, year].join('/');
 }
-function getMonth() {
-    var date = $("#calendar").fullCalendar('getDate');
-    var month_int = date.getMonth();
-    console.log(month_int);
-    //you now have the visible month as an integer from 0-11
-}
+// function getMonth() {
+//     var date = $("#calendar").fullCalendar('getDate');
+//     var month_int = date.getMonth();
+//     console.log(month_int);
+//     //you now have the visible month as an integer from 0-11
+// }
