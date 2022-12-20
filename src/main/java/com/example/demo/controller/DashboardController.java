a2b891a6fd53465c4695bd53f10c16b6cca49402
nguyenthanhloc2609 <nguyenthanhloc2609@gmail.com>
2022-12-20 20:54:06 +0700

    import data from excel

--- src/main/java/com/example/demo/controller/DashboardController.java
+++ src/main/java/com/example/demo/controller/DashboardController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -16,10 +17,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/dashboard")
@@ -34,6 +33,11 @@ public class DashboardController {
     @GetMapping("/finance/date")
     public ResponseEntity<?> getFinanceOnDate(@RequestParam String date) {
         return ResponseEntity.ok(financeService.getFinanceOnDay(date));
+    }
+
+    @PostMapping("/import")
+    public ResponseEntity<?> importExcel(@RequestParam("file") MultipartFile file) {
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/excel")
